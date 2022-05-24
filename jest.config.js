fb21b2c7ccece7b9737b54c53786a589a67c6b24
oAllanWeb <allanweb019@gmail.com>
2022-05-24 15:24:12 -0300

    feat: style of cart screen and config coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,24 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
   ],
+  testRegex: ['/*.test.ts$', '/*.test.tsx$'],
+  collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageDirectory: 'test-coverage',
+  collectCoverageFrom: [
+    'src/components/**/*.ts(x)?',
+    'src/hooks/**/*.ts(x)?',
+    'src/screens/**/*.ts(x)?',
+    'src/utils/**/*.ts(x)?',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  moduleDirectories: ['node_modules', 'src'],
 };
--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -1,17 +1,62 @@
-import {Box, Button, Text, View} from 'native-base';
+import {Entypo} from '@expo/vector-icons';
+import {
+  AspectRatio,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from 'native-base';
 import React from 'react';
+import ToMoney from '../../utils/ToMoney';
 import useCart from './hooks/useCart';
 
 function Cart() {
-  const {cart, removeFromCart} = useCart();
+  const {cart, removeFromCart, amountTotal} = useCart();
   return (
-    <View>
-      {cart.map((product, index) => (
-        <Box key={product.id}>
-          <Text>{product.title}</Text>
-          <Button onPress={() => removeFromCart(index)}>Remove</Button>
-        </Box>
-      ))}
+    <View flex="1">
+      <ScrollView p="2">
+        {cart.map((product, index) => (
+          <HStack
+            background="white"
+            padding="2"
+            mb={2}
+            flex={1}
+            rounded="md"
+            key={product.id}>
+            <AspectRatio h="100%" ratio={4 / 3}>
+              <Image
+                source={{
+                  uri: product?.image,
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+            <View
+              flex="1"
+              background="white"
+              padding="2"
+              rounded="md"
+              key={product.id}>
+              <Text fontWeight="bold">{product.title}</Text>
+            </View>
+            <HStack w="50px">
+              <IconButton
+                onPress={() => removeFromCart(index)}
+                icon={<Icon as={Entypo} color="red.500" name="cross" />}
+              />
+            </HStack>
+          </HStack>
+        ))}
+      </ScrollView>
+      <View height="60px" p="2" justifyContent="center">
+        <Heading width="100%" textAlign="left">
+          Total: {ToMoney(amountTotal)}
+        </Heading>
+      </View>
     </View>
   );
 }
